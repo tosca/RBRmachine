9b3acb035814545dcccc708aabefb876b31de8f2
tosca ragnini <tosca.ragnini@gmail.com>
2018-04-08 02:48:33 -0500

    google drive link works

---
+++ RBRweb/RBRweb/js/gdrive.js
@@ -0,0 +1,10 @@
+﻿
+var urlParams = new URLSearchParams(window.location.search);
+var id = urlParams.get('id');
+
+var templateUrl = "https://drive.google.com/embeddedfolderview?id={googleFolderId}#grid";
+var url = templateUrl.replace("{googleFolderId}", id);
+
+console.log(url);
+$('#myDiv').text(url);
+$('#myIframe').attr('src', url);                                       
