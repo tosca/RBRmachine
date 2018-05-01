432a69a24e634de16fec3758ca882623a4e6f220
tosca ragnini <tosca.ragnini@gmail.com>
2018-05-01 10:10:11 -0500

    add urlParams

--- RBRweb/RBRweb/js/gdrive.js
+++ RBRweb/RBRweb/js/gdrive.js
@@ -1,6 +1,17 @@
-﻿
-var urlParams = new URLSearchParams(window.location.search);
-var id = urlParams.get('id');
+﻿$.urlParam = function (name) {
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    if (results == null) {
+        return null;
+    }
+    else {
+        return decodeURI(results[1]) || 0;
+    }
+}
+
+
+
+//var urlParams = new URLSearchParams(window.location.search);
+var id = $.urlParam('id');
 
 var templateUrl = "https://drive.google.com/embeddedfolderview?id={googleFolderId}#grid";
 var url = templateUrl.replace("{googleFolderId}", id);
